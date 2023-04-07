9db8df513acc47eb5745ccbca71c4813a121498c
Gabriel Panaitescu <124690179+dez4s@users.noreply.github.com>
2023-04-07 18:36:21 +0300

    Pass all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,30 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let phrase = '';
+    if (num >= 0) {
+    for (i = 1; i <= num; i++) {
+        phrase += string;
+    } 
+    return phrase
+    } else {
+        return 'ERROR';
+    }
 
 };
+
+repeatString('hey', 3);
+
+repeatString('hey', 10);
+
+repeatString('hey', 1);
+
+repeatString('hey', 0);
+
+repeatString('hey', -1);
+
+const number = Math.floor(Math.random() * 1000)
+repeatString('hey', number);
+
+repeatString('', 10);
 
 // Do not edit below this line
 module.exports = repeatString;
