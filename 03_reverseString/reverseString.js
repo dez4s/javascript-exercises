4add73644e0cea673e4407b0bd01ea3a76bc19c2
Gabriel Panaitescu <124690179+dez4s@users.noreply.github.com>
2023-04-10 01:03:10 +0300

    Output reverse string and pass all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
-};
+const reverseString = function(string) {
+    let reverseOutput = '';
+    for (i = string.length - 1; i >= 0; i--) {
+      reverseOutput += string.charAt(i);
+    }
+      return reverseOutput;
+    };
+reverseString('');
 
 // Do not edit below this line
 module.exports = reverseString;
