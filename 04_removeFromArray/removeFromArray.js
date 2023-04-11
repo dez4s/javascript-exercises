7d5ed32a7bfd01d324b2e1f8dfdc087f7bccfb6e
Gabriel Panaitescu <124690179+dez4s@users.noreply.github.com>
2023-04-12 00:28:32 +0300

    Pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -4,7 +4,7 @@ const removeFromArray = function(array) {
   label:
   for (i = 0; i < array.length; i++) { 
     for (j = 0; j < removeNr.length; j++) {
-      if (array[i] == removeNr[j]) {
+      if (array[i] === removeNr[j]) {
         continue label;
       }
     }
@@ -16,7 +16,16 @@ return finalArray;
   removeFromArray([1, 2, 3, 4], 3);
 
   removeFromArray([1, 2, 3, 4], 3, 2);
+
+  removeFromArray([1, 2, 3, 4], 7, "tacos");
+
+  removeFromArray([1, 2, 3, 4], 7, 2);
+
+  removeFromArray([1, 2, 3, 4], 1, 2, 3, 4);
   
+  removeFromArray(["hey", 2, 3, "ho"], "hey", 3);
+
+  removeFromArray([1, 2, 3], "1", 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
