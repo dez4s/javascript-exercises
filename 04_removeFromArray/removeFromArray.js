2a816b443fb4fdb1e9d25da9503503a99fd5110b
Gabriel Panaitescu <124690179+dez4s@users.noreply.github.com>
2023-04-12 00:05:09 +0300

    Pass first two tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+  let removeNr = Array.prototype.slice.call(arguments, 1);
+  let finalArray = [];
+  label:
+  for (i = 0; i < array.length; i++) { 
+    for (j = 0; j < removeNr.length; j++) {
+      if (array[i] == removeNr[j]) {
+        continue label;
+      }
+    }
+    finalArray.push(array[i]); 
+  }
+return finalArray;
+}
+  
+  removeFromArray([1, 2, 3, 4], 3);
 
-};
+  removeFromArray([1, 2, 3, 4], 3, 2);
+  
 
 // Do not edit below this line
 module.exports = removeFromArray;
