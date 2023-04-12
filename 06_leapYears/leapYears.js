f8772102e511dda777df9687a10dd7f0db9f2a60
Gabriel Panaitescu <124690179+dez4s@users.noreply.github.com>
2023-04-13 01:41:37 +0300

    Pass all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let leapYes = 0;
+    if (year % 4 == 0) leapYes++; 
+    if (year % 100 == 0) leapYes--;
+    if (year % 100 == 0 && year % 400 == 0) leapYes++;
+    if (leapYes == 1) return true; else return false;
+} 
 
-};
+leapYears(1996);
+leapYears(1997);
+leapYears(34992);
+leapYears(1900);
+leapYears(1600);
+leapYears(700);
 
 // Do not edit below this line
 module.exports = leapYears;
