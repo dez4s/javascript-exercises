2a52ae88811101f4901a7dc0c1c48a8ce5558300
Gabriel Panaitescu <124690179+dez4s@users.noreply.github.com>
2023-04-12 03:41:49 +0300

    Pass all tests with second method

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,19 +1,18 @@
-const sumAll = function(start, end) {
-    let sum = 0;
-    if (start < 0 || end < 0 || typeof(start) !== "number" || typeof(end) !== "number") {
-        return "ERROR";
-    } else {
-    if (start < end) {
-        for (i = start; i <= end; i++) {
-        sum += Number(i);
-    }} 
-    else {
-    for (i = start; i >= end; i--) {
-        sum += Number(i);
+const sumAll = function(min, max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return 'ERROR';
+    if (min < 0 || max < 0) return 'ERROR';
+    if (min > max) {
+        let temp = min;
+        min = max;
+        max = temp;
     }
-    }}
+    let sum = 0;
+    for (i = min; i <= max; i++) {
+        sum += i;
+    }
     return sum;
-}
+};
+
 
 sumAll(1, 4);
 
