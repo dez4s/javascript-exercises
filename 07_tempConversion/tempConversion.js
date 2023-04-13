0bc9d590bc431caa52f20804e13644a9dd6721a5
Gabriel Panaitescu <124690179+dez4s@users.noreply.github.com>
2023-04-13 23:17:36 +0300

    Pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  return parseFloat(((x - 32) * 5 / 9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(y) {
+  return parseFloat(((y * 9 / 5) + 32).toFixed(1));
 };
+
+convertToCelsius(10);
+convertToCelsius(100);
+convertToCelsius(-32);
+
+convertToFahrenheit(0);
+convertToFahrenheit(73.2);
+convertToFahrenheit(10);
 
 // Do not edit below this line
 module.exports = {
