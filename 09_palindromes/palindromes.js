a5148bf312b59ced0772cbc6894dae35f0fbd683
Gabriel Panaitescu <124690179+dez4s@users.noreply.github.com>
2023-08-10 00:10:48 +0300

    Pass tests in palindromes.spec.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
+function palindromes(string) {
+    const stripedString = string.replace(/[^\w\s]|_/g, "").replace(/\s+/g, "").toLowerCase();
 
-};
+    if (stripedString == stripedString.split('').slice().reverse().join('')) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 // Do not edit below this line
 module.exports = palindromes;
