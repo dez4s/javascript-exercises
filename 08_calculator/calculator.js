013f5764a6610b63d61975a85103123afecbb765
Gabriel Panaitescu <124690179+dez4s@users.noreply.github.com>
2023-08-09 03:43:51 +0300

    Finish all tests in calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -7,23 +7,24 @@ const subtract = function(a, b) {
 };
 
 const sum = function(array) {
-  return array.reduce((accumulator, elem) => {
-    return accumulator + elem;
-  }, 0)
+  return array.reduce((accumulator, elem) => accumulator + elem, 0)
 };
 
 const multiply = function(array) {
-  return array.reduce((accumulator, elem) => {
-    return accumulator * elem;
-  }, 1)
+  return array.reduce((accumulator, elem) =>  accumulator * elem, 1)
 };
 
 const power = function(a, b) {
   return a ** b;
 };	
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a == 0) return 1;
+	let result = 1;
+  for (let i = 1; i <= a; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
