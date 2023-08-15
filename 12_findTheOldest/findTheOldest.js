aa05e4ec117ef985d0a59bef3076bd31a79a4892
Gabriel Panaitescu <124690179+dez4s@users.noreply.github.com>
2023-08-15 15:00:58 +0300

    Pass all tests in findTheOldest.spec.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    return array.reduce((accumulator, elem) => {
+        const currentOldest = calcAge(accumulator);
+        const currentElemAge = calcAge(elem); 
 
-};
+        return currentElemAge > currentOldest ? elem : accumulator
+    })
+}
+
+
+function calcAge(person) {
+    const currentYear = new Date().getFullYear();
+    if (!person.yearOfDeath) return currentYear - person.yearOfBirth;
+    return person.yearOfDeath - person.yearOfBirth;
+}
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
